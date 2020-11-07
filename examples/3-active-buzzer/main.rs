b11bc1f423f7851a70aa758b8c2f8c85cfff66e6
sugarraysam <samuel.blaisdowdy@protonmail.com>
2020-11-07 13:39:29 -0600

    refactor example 3 to use FnMut closure iterators

--- examples/3-active-buzzer/main.rs
+++ examples/3-active-buzzer/main.rs
@@ -12,14 +12,13 @@ fn main() -> ! {
     let dp = arduino_uno::Peripherals::take().unwrap();
     let pins = arduino_uno::Pins::new(dp.PORTB, dp.PORTC, dp.PORTD);
 
-    // will control the LED with on/off buttons
     let mut buzz_ctrl = PassiveBuzzerCTRL::new(pins);
     buzz_ctrl.start();
 }
 
 struct PassiveBuzzerCTRL {
     out: PB4<Output>,
-    iterations: u16,
+    iterations: &'static [u32],
     delays: &'static [u16],
 }
 
@@ -27,23 +26,35 @@ impl PassiveBuzzerCTRL {
     fn new(mut pins: arduino_uno::Pins) -> Self {
         PassiveBuzzerCTRL {
             out: pins.d12.into_output(&mut pins.ddr),
-            iterations: 1000,
-            delays: &[1, 2, 3, 100],
+            iterations: &[10, 100],
+            delays: &[0, 1, 2, 50, 100, 250],
         }
     }
     fn start(&mut self) -> ! {
-        let mut curr = 0;
+        // using closure iterators because vec! macro and most rust
+        // iterator constructs require std lib, which we dont have access
+        // same thing for the external rng crate
+        let mut next_n = closure_iterator(self.iterations);
+        let mut next_delay = closure_iterator(self.delays);
         loop {
-            self.buzz(self.delays[curr % self.delays.len()]);
-            curr += 1;
+            self.buzz(next_n(), next_delay());
         }
     }
-    fn buzz(&mut self, delay: u16) {
-        for _ in 0..self.iterations {
+    fn buzz(&mut self, n: u32, delay: u16) {
+        for _ in 0..n {
             self.out.set_high().void_unwrap();
             arduino_uno::delay_ms(delay);
             self.out.set_low().void_unwrap();
             arduino_uno::delay_ms(delay);
         }
+    }
+}
+
+fn closure_iterator<'a, T: Copy>(values: &'a [T]) -> impl FnMut() -> T + 'a {
+    let mut next = 0;
+    move || -> T {
+        let choice = values[next % values.len()];
+        next += 1;
+        choice
     }
 }
